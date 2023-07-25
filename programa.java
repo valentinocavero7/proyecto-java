b2ba62d56d8376752c10156e8a54a176e2a3502f
valentinocavero7 <111661567+valentinocavero7@users.noreply.github.com>
2023-07-24 22:56:01 -0500

    confirmar logro xd

--- programa.java
+++ programa.java
@@ -1,5 +1,7 @@
 public class programa {
     public static void main(String[] args) {
      System.out.println("Hola mundo");
+     System.out.println("Funcionó!");   
     }
 }
+// mensaje de confirmación de que hemos podido agregar un archivo a nuestro repositorio
